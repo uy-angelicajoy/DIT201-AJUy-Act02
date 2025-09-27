091df1641f73eb3b2b284ab54a2d58b15ec647c1
uy-angelicajoy <angelicajoyuy16@gmail.com>
2025-09-28 00:03:20 +0800

    upload mobile app second activity

---
+++ app/src/main/java/com/example/multihello/SecondActivity.kt
@@ -0,0 +1,30 @@
+package com.example.multihello
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_second)
+
+        val btnNavigate2: Button = findViewById<Button>(R.id.btnNavigate2)
+        btnNavigate2.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
